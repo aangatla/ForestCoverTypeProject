e4698564070e13f6fc958a33d950a000de0b60a3
Sudeep Tiwari <95038751+aangatla@users.noreply.github.com>
2023-04-26 23:05:15 +0530

    Update config_entity.py

--- ForestCoverPrediction/entity/config_entity.py
+++ ForestCoverPrediction/entity/config_entity.py
@@ -23,7 +23,7 @@ class DataIngestionConfig:
 
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         try:
-            self.data_file_path = r"ForestCoverTypeProject\forest_cover.csv"
+            self.data_file_path = r"notebook/train.csv"
             self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
             self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
             self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
